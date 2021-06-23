205ae1ecedeb66e83442331b67e95f843f629cea
igor ogai <ogayigore1996@mail.ru>
2021-06-23 16:04:15 +0300

    Initial Commit

---
+++ MedDesktop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedDesktop
+//
+//  Created by Igor Ogai on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
